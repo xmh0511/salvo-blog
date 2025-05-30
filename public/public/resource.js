822332f828c46db4a47b073268f601dcbf3d632f
xmh0511 <970252187@qq.com>
2025-05-30 16:39:56 +0800

    update resource.js

--- public/public/resource.js
+++ public/public/resource.js
@@ -1,5 +1,10 @@
 
+// editormd.katexURL = {
+// 	css : "http://localhost:8080/public/katex/katex.min",
+// 	js  : "http://localhost:8080/public/katex/katex.min"   
+// }
+
 editormd.katexURL = {
-	css : "http://localhost:8080/public/katex/katex.min",
-	js  : "http://localhost:8080/public/katex/katex.min"   
+	css : "/public/katex/katex.min",
+	js  : "/public/katex/katex.min"   
 }
